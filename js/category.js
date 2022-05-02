98d8b4cd0409567174ac93645d11ae42c0323bfb
Maria <mrmejia@scl.edu.gt>
2022-05-02 17:38:37 -0600

    Se agrega administración de datos de la empresa Alibaba

---
+++ js/category.js
@@ -0,0 +1,10 @@
+let category = [
+    /**CATEGORY_ID , CATEGORY_NAME */
+    [    4000,        'Laptops']
+    [    4100,	      'Mobile phones']
+    [    4200,	      'Tablets']
+    [    4300,	      'Servers']
+    [    4400,	      'PC']
+    [    4500,	      'Cables']
+    [    4600,	      'Headphones']
+]
